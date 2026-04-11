e8f56f6521d071394bdfc300a5f76e03d2ee3021
Kaue Marcelo Alves <165342157+KAUEsevla@users.noreply.github.com>
2026-04-10 22:00:46 -0300

    Update email configuration to remove password
    Removed the password from the email configuration for security.

--- configEmail.php
+++ configEmail.php
@@ -13,7 +13,7 @@ function configurarEmail() {
     $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'kaue.univillegsr@gmail.com';         // Seu Gmail
-    $mail->Password   = 'hnkczzerrqcdlgjj';         // A SENHA DE 16 DÍGITOS
+    $mail->Password   = '';         // A SENHA DE 16 DÍGITOS
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; 
     $mail->Port       = 587;
 
